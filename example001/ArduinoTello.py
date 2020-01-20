8ef551ee54983886d477bf56bd381aa6867de737
Martin P <43763327+cozmobotics@users.noreply.github.com>
2020-01-20 11:51:35 +0100

    some bugfixes and better usability

--- example001/ArduinoTello.py
+++ example001/ArduinoTello.py
@@ -1,3 +1,7 @@
+
+'''A simple program to fly the Tello quadrocopter (drone) with a controller that sends data via serial (tty) interface
+   See https://github.com/cozmobotics/ArduinoTello/ for details '''
+
 import serial
 import threading
 import time
@@ -7,7 +11,7 @@ import sys
 
 Running = True
 try:
-	ser = serial.Serial('COM13', 115200 ) # Establish the connection on a specific port
+	ser = serial.Serial('COM13', 115200, timeout = 1) # Establish the connection on a specific port
 except Exception as e:
 	print (str(e))
 	sys.exit()
@@ -26,17 +30,20 @@ Command = ''
 #---------------------------------------------------------------------------
 def recv():
 	global sock
+	global Running
 	
 	print ("UDP receive task started")
 	
 	count = 0
-	while True: 
+	while Running: 
 		try:
 			data, server = sock.recvfrom(1518)
 			print(data.decode(encoding="utf-8"))
 		except Exception:
 			print ('\nExit . . .\n')
 			break
+			
+	print ("UDP receive task ended")
 
 #---------------------------------------------------------------------------
 def getCommandSerial():
@@ -57,35 +64,30 @@ def getCommandSerial():
 			print (Text)
 			Text = Text.encode(encoding="utf-8") 
 			sent = sock.sendto(Text, tello_address)
-		else:
-			print ('.', end='')
+	print ("Serial task ended")
 
 #---------------------------------------------------------------------------
-def dummyFunc():
-	global Running
-	while Running:
-		print ("main loop still running")
-		time.sleep (10)
 
-#---------------------------------------------------------------------------
-# dummyTask = threading.Thread(target=dummyFunc)
-# dummyTask.start()
-
-#recvThread create
+# create threads
 recvThread = threading.Thread(target=recv)
 recvThread.start()
 
-
-
 serialTask = threading.Thread(target=getCommandSerial)
 serialTask.start()
-# getCommandSerial()
 
+print ("Here you see the commands that comr from the serial line. ")
+print ("You can type additional commands like \"battery?\".")
+print ("Type \"end\" to quit the program.")
 
 while (Running):
-	# Command = input()
-	# print (Command)
-	# if (Command == "end"):
-		# Running = False
-	time.sleep (10)
+	Command = input()
+	print (Command)
+	if (Command == "end"):
+		Running = False
+	else:
+		Command = Command.encode(encoding="utf-8")
+		sent = sock.sendto(Command, tello_address)
 
+print ("ArduinoTello will end in 3 seconds")
+time.sleep(3)
+print ("Thank you for using ArduinoTello")
